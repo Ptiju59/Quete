dc9e45e7a29bc7128b78ede8ceef688ec686b986
Julien L <julien.lefevre@decathlon.com>
2024-12-05 15:21:15 +0100

    Update Quete3.py

--- Quete3.py
+++ Quete3.py
@@ -1,89 +1,53 @@
 import streamlit as st
 from streamlit_authenticator import Authenticate
+import yaml
+from yaml.loader import SafeLoader
+import streamlit_authenticator as stauth
 
-# Nos données utilisateurs doivent respecter ce format
+with open('/Users/ju/Downloads//Forma_Data/Visual/config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
 
-lesDonneesDesComptes = {'usernames': {'utilisateur': {'name': 'utilisateur',
-   'password': 'utilisateurMDP',
-   'email': 'utilisateur@gmail.com',
-   'failed_login_attemps': 0, # Sera géré automatiquement
-   'logged_in': False, # Sera géré automatiquement
-   'role': 'utilisateur'},
-  'root': {'name': 'root',
-   'password': 'rootMDP',
-   'email': 'admin@gmail.com',
-   'failed_login_attemps': 0, # Sera géré automatiquement
-   'logged_in': False, # Sera géré automatiquement
-   'role': 'administrateur'}}}
+# Pre-hashing all plain text passwords once
+# stauth.Hasher.hash_passwords(config['credentials'])
 
-authenticator = Authenticate(
-    lesDonneesDesComptes, # Les données des comptes
-    "cookie name", # Le nom du cookie, un str quelconque
-    "cookie key", # La clé du cookie, un str quelconque
-    30, # Le nombre de jours avant que le cookie expire 
-)
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'])
+
+
+
+
 
 authenticator.login()
+if st.session_state['authentication_status']:
+    
+    menu_option = st.sidebar.selectbox(
+        "Sélectionner une option",
+        ["Accueil", "Photos"],
+        key='menu_selectbox')
+    
+    if menu_option == "Accueil":
+        st.title("Bienvenue sur la page d'accueil")
+        st.image("https://www.stylepochoir.fr/medias/images/d3201-logo-football-club-losc-lille-pochoir.jpg")
+        authenticator.logout()
+    elif menu_option == "Photos":
+        st.title("Evolution des 3 derniers Logo du LOSC")
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            st.header("2002")
+            st.image("losc1.jpg")
 
-def accueil():
-      st.title("Bienvenu sur le contenu réservé aux utilisateurs connectés")
+        with col2:
+            st.header("2012")
+            st.image("losc2.jpg")
 
+        with col3:
+            st.header("2018")
+            st.image("losc3.jpg")  
 
-if st.session_state["authentication_status"]:
-  accueil()
-  # Le bouton de déconnexion
-  authenticator.logout("Déconnexion")
-
-elif st.session_state["authentication_status"] is False:
-    st.error("L'username ou le password est/sont incorrect")
-elif st.session_state["authentication_status"] is None:
-    st.warning('Les champs username et mot de passe doivent être remplie')
-
-import streamlit as st
-# Importation du module
-from streamlit_option_menu import option_menu
-
-# Création du menu qui va afficher les choix qui se trouvent dans la variable options
-selection = option_menu(
-            menu_title=None,
-            options = ["Accueil", "Photos"]
-        )
-
-# On indique au programme quoi faire en fonction du choix
-if selection == "Accueil":
-    st.write("Bienvenue sur la page d'accueil !")
-elif selection == "Photos":
-    st.write("Bienvenue sur mon album photo")
-# ... et ainsi de suite pour les autres pages
-
-import streamlit as st
-
-col1, col2, col3 = st.columns(3)
-
-with col1:
-  st.header("A cat")
-  st.image("https://static.streamlit.io/examples/cat.jpg")
-
-with col2:
-  st.header("A dog")
-  st.image("https://static.streamlit.io/examples/dog.jpg")
-
-with col3:
-  st.header("An owl")
-  st.image("https://static.streamlit.io/examples/owl.jpg")
-  
-  import streamlit as st
-
-# Using object notation
-add_selectbox = st.sidebar.selectbox(
-    "How would you like to be contacted?",
-    ("Email", "Home phone", "Mobile phone")
-)
-
-# Using "with" notation
-with st.sidebar:
-    add_radio = st.radio(
-        "Choose a shipping method",
-        ("Standard (5-15 days)", "Express (2-5 days)")
-    )
-  
+elif st.session_state['authentication_status'] is False:
+        st.error('Username/password is incorrect')
+elif st.session_state['authentication_status'] is None:
+        st.warning('Please enter your username and password')
